2190ad8d69d900ffb887553590586f1b2ff7343d
Tiago Machado <tiago.o.machado@gmail.com>
2020-04-13 02:22:10 -0300

    icones no cabecalho e logo

--- src/template/aside/MenuHeader.jsx
+++ src/template/aside/MenuHeader.jsx
@@ -2,5 +2,8 @@ import React from 'react';
 import './MenuHeader.css';
 
 export default props => (
-    <div className="menuHeader"></div>
+    <div className="menuHeader">
+        <img src="assets/SocialNetworkLogoIcon.png" alt="logo-icon" width="50" />
+        <img src="assets/SocialNetworkLogo.png" alt="logo-icon" width="180" />
+    </div>
 );
--- src/template/header/Header.jsx
+++ src/template/header/Header.jsx
@@ -2,5 +2,13 @@ import React from 'react';
 import './Header.css';
 
 export default props => (
-    <header></header>
+    <header className="header">
+        <div className="menuToggle"><i className="fa fa-bars fa-2x"></i></div>
+        <div className="headerOptions">
+            <div className="headerOptionsIcon"><i className="fa fa-home fa-2x"></i></div>
+            <div className="headerOptionsIcon"><i className="fa fa-users fa-2x"></i></div>
+            <div className="headerOptionsIcon"><i className="fa fa-envelope fa-2x"></i></div>
+            <div className="headerOptionsIcon"><i className="fa fa-user fa-2x"></i></div>
+        </div>
+    </header>
 );
